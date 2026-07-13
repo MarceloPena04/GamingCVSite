c55e5dfc17dc9b0bafb96d04c67d28114a6be3fa
a01722831 <a01722831@tec.mx>
2026-07-13 12:10:17 -0600

    Missing hyper links for social media

--- src/data/profile.js
+++ src/data/profile.js
@@ -5,7 +5,7 @@ export const profile = {
     'I design and build games end-to-end, gameplay systems, networking, and the art pipeline in between.',
   location: 'Monterrey, MX', // "" to hide
   email: 'penamarcelo04@gmail.com',
-  github: 'https://github.com/YOUR_GITHUB', // "" to hide
-  linkedin: 'https://www.linkedin.com/in/YOUR_LINKEDIN', // "" to hide
+  github: 'https://github.com/a01722831', // "" to hide
+  linkedin: 'https://linkedin.com/in/marcelopena04', // "" to hide
   cvUrl: '', // optional: link to a hosted PDF of your CV
 }
